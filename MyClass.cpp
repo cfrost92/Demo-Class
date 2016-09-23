8de7b9792e92737be49731ae20bc9f4996938f7e
cfrost92 <christopherrj37@gmail.com>
2016-09-23 01:53:21 -0400

    Filled in missing parts of Implementation file and Client file

--- MyClass.cpp
+++ MyClass.cpp
@@ -1,1 +1,37 @@
+//Implementation file
+
+#include<iostream>
+using namespace std;
+#include "MyClass.h"
+
+void MyClass::setNum (int v)
+{
+	num=v;
+}
+
+int MyClass::getNum ()
+{
+	return num;
+}
+
+void MyClass::setRoom (string x)
+{
+	room = x;
+}
+
+string MyClass::getRoom()
+{
+	return room;
+}
+
+void MyClass::setFun(bool y)
+{
+	fun = y;
+}
+
+bool MyClass::getFun()
+{
+	return fun;
+}
+
 
--- main.cpp
+++ main.cpp
@@ -2,9 +2,18 @@
 
 #include <iostream>
 using namespace std;
-
+#include "MyClass.h" 
 
 int main()
 {
+	int getNum();
+
+	void setRoom(string v);
+	int getRoom();
+	string getRoom();
+
+	void setFun(bool y);
+	int getFun();
+	bool getFun();
 
 }
