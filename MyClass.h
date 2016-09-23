1ae9aa1d2372084c80a2dcca9ea48b5fcc52cf8c
cfrost92 <christopherrj37@gmail.com>
2016-09-23 01:35:25 -0400

    Filled in missing parts of header

--- MyClass.h
+++ MyClass.h
@@ -1,1 +1,18 @@
+//This is my header file
+#include<string>
+using std::string;
 
+class MyClass
+{
+public:
+	void setNum(int);
+	int getNum();
+	void setRoom(string);
+	string getRoom();
+	void setFun(bool);
+	bool getFun();
+private:
+	int num;
+	string room;
+	bool fun;
+};//end class
