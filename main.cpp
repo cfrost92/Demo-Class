affe7b14f966fdef80ac3e90d079dd95e1e833a8
cfrost92 <christopherrj37@gmail.com>
2016-09-29 21:05:39 -0400

    Changed main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,41 @@
-//complete me
+//Main.cpp
+//Client File
 
-#include <iostream>
-using namespace std;
-#include "MyClass.h" 
+ #include "MyClass.h"
+ #include <string>
+ #include <iostream>
 
-int main()
-{
-	int getNum();
+ using namespace std;
 
-	void setRoom(string v);
-	int getRoom();
-	string getRoom();
 
-	void setFun(bool y);
-	int getFun();
-	bool getFun();
+ int main()
+ {
+	myClass myVar;
+	int var = 10;
+	string str;
+	MyClass myVar;
+	//int var = 10;
+	//string str;
 
+	cin >> var;
+	cout << "var = " << var < endl;
+	/*cin >> var;
+	cout << "var = " << var << endl;
+	cin >> str;
+	cout << "str = " << str;
+	cout << "str = " << str << endl; */
+
+ 	myVar.setNum(32);
+	myVar.getNum();
+	cout<< myVar.getNum()<< endl;
+	myVar.setRoom("LR 12");
+	myVar.getRoom();
+	myVar.setFun('True');
+	myVar.getFun();
+	cout<<myVar.getRoom()<< endl;
+	myVar.setFun(true);
+	cout<<myVar.getFun()<<endl;
+
+
+	cout<< myVar;
 }
